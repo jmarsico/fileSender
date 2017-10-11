1a5b07527eabb3fbe1c2810e08316e326ab7d175
Ultra Low Res Studuio <ultralowres@Ultras-Mac-Pro.fios-router.home>
2017-10-11 11:53:24 -0400

    first commit

---
+++ fileSender.js
@@ -0,0 +1,40 @@
+var client = require('scp2');
+var chokidar = require('chokidar');
+var fs = require("fs");
+
+
+
+
+var watcher = chokidar.watch('./tmp', {
+  ignored: /(^|[\/\\])\../,
+  persistent: true
+});
+
+var log = console.log.bind(console);
+
+
+//setup the listener
+watcher
+	.on('add', path => {
+		// stuff to do when a new file comes in
+		log("new file: " + path)
+		var newFile = path;
+
+		var file = fs.readFileSync("config.json");
+		var pijson = JSON.parse(file);
+
+		var pathOnPi = pijson["path"];
+
+		
+		
+		var numPis = pijson["rpis"].length;
+		
+		for(var i = 0; i < numPis; i++){
+			var piPath = 'pi:raspberry@';
+			piPath += pijson["rpis"][i];
+			piPath += pathOnPi;
+
+			log(piPath);
+			client.scp(, piPath, function(err){});
+		}
+	});
